c2ac0c2ef42f6d4635212075e5fb3082c909177e
Yassine Derouich <yassine.derouich@edu.esiee.fr>
2025-10-05 15:58:36 +0000

    fin exercice 4 primes

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
+""" Module fournissant la fonction racine carré SQRT """
 from math import sqrt
 
 #### Fonction secondaire
 
 
-def isprime(p):
+def isprime(p: int)->bool:
+    """ 
+    Déterminer si un nombre est premier.
 
-    # votre code ici
+    args:
+    p (entier): nombre à vérifier sa primalité
 
-    pass
+    returns:
+    bool: True si p est premier et False sinon
+    """
+    if p == 1:
+        return False
+    if p == 2:
+        return True
+    limite_diviseur = int(sqrt(p))+1
+    for diviseur in range(2, limite_diviseur):
+        if p % diviseur == 0:
+            return False
+    return True
 
 #### Fonction principale
 
-
 def main():
+    """
+    Indique si les nombres n de a à b sont primaire ou non
 
-    # vos appels à la fonction secondaire ici
+    Args:
+        None
 
-    for n in range(100):
+    Returns:
+        None mais appelle isprime(n) qui retourne un bool
+    """
+    a: int = 2
+    b: int = 45
+    for n in range(a,b):
         if isprime(n):
-            print(n, end=", ")
+            print(f"{n} est un nombre premier", end=", ")
 
     print()
 
